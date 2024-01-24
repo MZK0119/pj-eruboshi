d794b26b738d684f4a832da0b2191e4e79915ecd
MZK0119 <157603009+MZK0119@users.noreply.github.com>
2024-01-24 22:26:55 +0900

    Update pj.js

--- pj.js
+++ pj.js
@@ -1,20 +1,26 @@
-let accordionDetails = '.js-details';
-let accordionSummary = '.js-details-summary';
-let accordionContent = '.js-details-content';
-let speed = 300
+ let accordionDetails = '.js-details';
+    let accordionSummary = '.js-details-summary';
+    let accordionContent = '.js-details-content';
+    let speed = 500;
 
-$(accordionSummary).each(function() {
-  $(this).on("click", function(event) {
-    event.preventDefault();
-    $(this).toggleClass("is-active");
-
-    if ($(this).parent($(accordionDetails)).attr("open")) {
-      $(this).nextAll($(accordionContent)).slideUp(speed, function() {
-        $(this).parent($(accordionDetails)).removeAttr("open");
-      });
-    } else {
-      $(this).parent($(accordionDetails)).attr("open", "true");
-      $(this).nextAll($(accordionContent)).hide().slideDown(speed);
-    }
-  })
-})
+    $(accordionSummary).each(function() {
+      $(this).on("click", function(event) {
+        // summaryにis-activeクラスを切り替え
+        $(this).toggleClass("is-active");
+        // デフォルトの挙動を無効化
+        event.preventDefault();
+        if ($(this).parent($(accordionDetails)).attr("open")) {
+          // アコーディオンを閉じるときの処理
+          $(this).nextAll($(accordionContent)).slideUp(speed, function() {
+            // アニメーションの完了後にopen属性を取り除く
+            $(this).parent($(accordionDetails)).removeAttr("open");
+          });
+        } else {
+          // アコーディオンを開くときの処理
+          // open属性を付ける
+          $(this).parent($(accordionDetails)).attr("open", "true");
+          // いったんdisplay:none;してからslideDownで開く
+          $(this).nextAll($(accordionContent)).hide().slideDown(speed);
+        }
+      })
+    })
